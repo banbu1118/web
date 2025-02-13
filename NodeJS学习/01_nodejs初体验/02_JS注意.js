78745fe7e981fc31a232a60a458251bcf660834e
banbu1118 <1902802324@qq.com>
2025-02-14 01:13:17 +0800

    update

---
+++ NodeJS学习/01_nodejs初体验/02_JS注意.js
@@ -0,0 +1,24 @@
+//BOM
+// console.log(window);
+
+//DOM
+// console.log(document);
+
+
+// console.log("i love you");
+
+// setTimeout(() => {
+//    console.log("love too");
+    
+// }, 1000);
+
+//global顶级对象
+// console.log(global);
+
+// console.log(globalThis);
+
+console.log(globalThis === global);
+
+
+
+
