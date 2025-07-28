e5dbc5d9c576344b972b88cebc21a9a4b18df2ff
banbu1118 <1902802324@qq.com>
2025-07-28 20:21:12 +0800

    update

--- express快速入门/demo/index.js
+++ express快速入门/demo/index.js
@@ -2,14 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.get('/login', (req, res) => {
-    res.send('表单页面')
+app.get('/home', (req, res) => {
+    res.send('前台首页')
 });
 
-app.post('/login', (req, res) => {
-    res.send('获取用户数据')
+app.get('/search', (req, res) => {
+    res.send('内容搜索')
 });
 
+app.get('/admin', (req, res) => {
+    res.send('首台首页')
+});
+app.get('/setting', (req, res) => {
+    res.send('设置页面')
+});
 
 app.listen(3000, () => {
     console.log('服务已启动，端口 3000');
